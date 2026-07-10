f328602756fe626c1a642d49529f6396827a46e9
Abhay999-web <singhabhaypratap609@gmail.com>
2026-07-10 21:14:08 +0530

    Improving some lil bugs

--- Frontend/src/features/chat/pages/Dashboard.jsx
+++ Frontend/src/features/chat/pages/Dashboard.jsx
@@ -7,15 +7,11 @@ const Dashboard = () => {
 
   return (
     <main className='h-[100dvh] w-full bg-[#171717] flex overflow-hidden font-sans text-gray-200 antialiased selection:bg-[#2f2f2f]'>
-      
-     {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-    {/* Chat Area */}
+      
       <div className="flex-1 flex flex-col min-w-0 h-full min-h-0">
         <ChatArea sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       </div>
-
     </main>
   )
 }
